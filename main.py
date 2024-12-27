44e0052b5a26a5b753fc5f8a7706b089240c5932
Mabo Giqwa <122114072+mabogiqwa@users.noreply.github.com>
2024-12-27 19:15:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -97,31 +97,255 @@ cosmeticsFrame.grid(row=0, column=0)
 #Elements in frame
 
 #Bath soap label and entry
-bathSoapLabel=Label(cosmeticsFrame,text='Bath Soap',font=('Helvetica',15,'bold'),
+bathSoapLabel=Label(cosmeticsFrame,text='Bath Soap:',font=('Helvetica',15,'bold'),
                     bg='#36454F',
                     fg='white'
                    )
-bathSoapLabel.grid(row=0, column=0, pady=9, padx=10)
+bathSoapLabel.grid(row=0,column=0,pady=9,padx=2)
 
 bathSoapEntry=Entry(cosmeticsFrame,
                     font=('Helvetica',15,'bold'),
                     width=10,
                     bd=5)
-bathSoapEntry.grid(row=0, column=1, pady=9, padx=10)
+bathSoapEntry.grid(row=0,column=1,pady=9,padx=2)
 
 #Cleanser soap label and entry
 cleanserLabel=Label(cosmeticsFrame,
-                    text='Cleanser',
+                    text='Cleanser:',
                     font=('Helvetica',15,'bold'),
                     bg='#36454F',
                     fg='white'
                    )
-cleanserLabel.grid(row=1, column=0, pady=9, padx=10)
+cleanserLabel.grid(row=1,column=0,pady=9,padx=2)
+
 cleanserEntry=Entry(cosmeticsFrame,
                     font=('Helvetica',15,'bold'),
                     width=10,
                     bd=5
                    )
-cleanserEntry.grid(row=1, column=1, pady=9, padx=10)
+cleanserEntry.grid(row=1,column=1,pady=9,padx=2)
+
+#Cologne label and entry
+cologneLabel=Label(cosmeticsFrame,
+                   text="Cologne:",
+                   font=('Helvetica',15,'bold'),
+                   bg='#36454F',
+                   fg='white'
+                  )
+cologneLabel.grid(row=2,column=0,pady=9,padx=2)
+
+cologneEntry=Entry(cosmeticsFrame,
+                   font=('Helvetica',15,'bold'),
+                   width=10,
+                   bd=5
+                  )
+cologneEntry.grid(row=2,column=1,pady=9,padx=2)
+
+#Body lotion label and entry
+bodyLotionLabel=Label(cosmeticsFrame,
+                 text='Body lotion:',
+                 font=('Helvetica',15,'bold'),
+                 bg='#36454F',
+                 fg='white'
+                )
+bodyLotionLabel.grid(row=3,column=0,pady=9,padx=2)
+
+bodyLotionLabelEntry=Entry(cosmeticsFrame,
+                           font=('Helvetica',15,'bold'),
+                           width=10,
+                           bd=5
+                           )
+bodyLotionLabelEntry.grid(row=3,column=1,pady=9,padx=10)
+
+#Shampoo label and entry
+shampooLabel=Label(cosmeticsFrame,
+                   text='Shampoo:',
+                   font=('Helvetica',15,'bold'),
+                   bg='#36454F',
+                   fg='white'
+                   )
+shampooLabel.grid(row=4,column=0,pady=9,padx=2)
+
+shampooLabelEntry=Entry(cosmeticsFrame,
+                        font=('Helvetica',15,'bold'),
+                        width=10,
+                        bd=5
+                        )
+shampooLabelEntry.grid(row=4,column=1,pady=9,padx=2)
+
+#Toothpaste
+toothPasteLabel=Label(cosmeticsFrame,
+                      text='Toothpaste:',
+                      font=('Helvetica',15,'bold'),
+                      bg='#36454F',
+                      fg='white'
+                      )
+toothPasteLabel.grid(row=5,column=0,pady=9,padx=2)
+
+toothPasteEntry=Entry(cosmeticsFrame,
+                      font=('Helvetica',15,'bold'),
+                      width=10,
+                      bd=5
+                      )
+toothPasteEntry.grid(row=5,column=1,pady=9,padx=2)
+
+#Meat and Sea-food block of code
+meatFrame=LabelFrame(productsFrame,
+                          text='Meat and Seafood',
+                          font=('Helvetica',15,'bold'),
+                          bg='#36454F',
+                          fg='#FFEB99',
+                          bd=8,
+                          relief=GROOVE
+                          )
+meatFrame.grid(row=0,column=1)
+
+#Fresh meat
+freshMeatLabel=Label(meatFrame,
+                     text='Fresh Meat:',
+                     font=('Helvetica',15,'bold'),
+                     bg='#36454F',
+                     fg='white',
+                     )
+freshMeatLabel.grid(row=0,column=0,pady=9,padx=2)
+
+freshMeatEntry=Entry(meatFrame,
+                     font=('Helvetica',15,'bold'),
+                     width=10,
+                     bd=5
+                     )
+freshMeatEntry.grid(row=0,column=1,pady=9,padx=10)
+
+#Processed meat
+processedMeatLabel=Label(meatFrame,
+                         font=('Helvetica',15,'bold'),
+                         text='Processed Meat:',
+                         bg='#36454F',
+                         fg='white'
+                         )
+processedMeatLabel.grid(row=1,column=0,pady=9,padx=2)
+
+processedMeatEntry=Entry(meatFrame,
+                              font=('Helvetica',15,'bold'),
+                              width=10,
+                              bd=5
+                              )
+processedMeatEntry.grid(row=1,column=1,pady=9,padx=2)
+
+#Fresh seafood
+freshSeaFoodLabel=Label(meatFrame,
+                        font=('Helvetica',15,'bold'),
+                        text='Fresh Sea Food:',
+                        bg='#36454F',
+                        fg='white'
+                        )
+freshSeaFoodLabel.grid(row=2,column=0,pady=9,padx=10)
+
+freshSeaFoodEntry=Entry(meatFrame,
+                        font=('Helvetica',15,'bold'),
+                        width=10,
+                        bd=5
+                        )
+freshSeaFoodEntry.grid(row=2,column=1,pady=9,padx=2)
+                
+#Frozen seafood
+frozenSeaFoodLabel=Label(meatFrame,
+                         font=('Helvetica',15,'bold'),
+                         text='Frozen Sea Food:',
+                         bg='#36454F',
+                         fg='white'
+                         )
+frozenSeaFoodLabel.grid(row=3,column=0,pady=9,padx=2)
+
+frozenSeaFoodEntry=Entry(meatFrame,
+                         font=('Helvetica',15,'bold'),
+                         width=10,
+                         bd=5
+                         )
+frozenSeaFoodEntry.grid(row=3,column=1,pady=9,padx=2)
+
+#Speciality meats
+specialityMeatsLabel=Label(meatFrame,
+                           font=('Helvetica',15,'bold'),
+                           text='Offal & Speciality Meats:',
+                           bg='#36454F',
+                           fg='white'
+                          )
+specialityMeatsLabel.grid(row=4,column=0,pady=9,padx=2)
+
+specialityMeatsEntry=Entry(meatFrame,
+                           font=('Helvetica',15,'bold'),
+                           width=10,
+                           bd=5
+                           )
+specialityMeatsEntry.grid(row=4,column=1,pady=9,padx=2)
+
+#Smoked meats
+smokedMeatsLabel=Label(meatFrame,
+                       text='Smoked meats:',
+                       font=('Helvetica',15,'bold'),
+                       bg='#36454F',
+                       fg='white'
+                       )
+smokedMeatsLabel.grid(row=5,column=0,pady=9,padx=2)
+
+smokedMeatsLabel=Entry(meatFrame,
+                       font=('Helvetica',15,'bold'),
+                       width=10,
+                       bd=5
+                       )
+smokedMeatsLabel.grid(row=5,column=1,pady=9,padx=2)
+
+#Beverages block of code (Frame)
+beveragesFrame=LabelFrame(productsFrame,
+                           text='Beverages',
+                           font=('Helvetica',15,'bold'),
+                           bg='#36454F',
+                           fg='#FFEB99',
+                           bd=8,
+                           relief=GROOVE
+                           )
+beveragesFrame.grid(row=0,column=2)
+
+#Water entry
+waterLabel=Label(beveragesFrame,
+                 text='Water:',
+                 font=('Helvetica',15,'bold'),
+                 bg='#36454F',
+                 fg='white',
+                 )
+waterLabel.grid(row=0,column=0,pady=9,padx=2)
+
+waterEntry=Entry(beveragesFrame,
+                 font=('Helvetica',15,'bold'),
+                 width=10,
+                 bd=5
+                 )
+waterEntry.grid(row=0,column=1,pady=9,padx=2)
+
+#Soft drinks entry
+softDrinksLabel=Label(beveragesFrame,
+                      font=('Helvetica',15,'bold'),
+                      text='Soft Drinks:',
+                      bg='#36454F',
+                      fg='white'
+                      )
+softDrinksLabel.grid(row=1,column=0,pady=9,padx=2)
+
+softDrinksEntry=Entry(beveragesFrame,
+                      font=('Helvetica',15,'bold'),
+                      width=10,
+                      bd=5
+                      )
+softDrinksEntry.grid(row=1,column=1,pady=9,padx=2)
+
+#Dairy beverages
+dairyLabel=Label(beveragesFrame,
+                 text='Dairy Beverages:',
+                 font=('Helvetica',15,'bold'),
+                 bg='#36454F',
+                 fg='white'
+                 )
+dairyLabel.grid(row=2,column=0,pady=9,padx=2)
 
 screen.mainloop()
